34a7db484f3179a95212f049b06447221b31f393
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2023-10-16 13:59:45 +0200

    feat : display some data into the header

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -11,11 +11,32 @@ const Info = () => {
     })();
   }, []);
 
-  console.log(headerData);
-
   return (
-    <div>
-      <h1>Header</h1>
+    <div className="flex flex-row w-[40%]">
+      <div className="w-[45%] border-2 rounded-lg border-cyan-500">
+        <ul className="flex flex-col">
+          <li className="text-center bg-gray-900 rounded-lg p-5 text-2xl font-bold text-cyan-500 mb-5">
+            Watch Tower
+          </li>
+          <li className="m-2 text-xs lg:text-xl">
+            Crypto :{" "}
+            <span className="text-green-400">
+              {headerData.active_cryptocurrencies}
+            </span>
+          </li>
+          <li className="m-2 text-xs lg:text-xl">
+            Le marché :{" "}
+            <span className="text-green-400">{headerData.markets}</span>
+          </li>
+        </ul>
+      </div>
+      <div className="flex items-center w[45%] border-2 rounded-lg border-cyan-500">
+        <ul className="flex flex-col gap-5 ">
+          <li>Global Market Cap :</li>
+          <li>BTC dominance :</li>
+          <li>ETH dominance :</li>
+        </ul>
+      </div>
     </div>
   );
 };
