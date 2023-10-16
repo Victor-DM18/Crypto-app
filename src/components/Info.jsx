f836fe981214ca71c097a85adb7de08e4edf591b
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2023-10-16 13:28:17 +0200

    feat : fetch data with axios useState & useEffect

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,4 +1,18 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 const Info = () => {
+  const [headerData, setHeaderData] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const res = await axios.get("https://api.coingecko.com/api/v3/global");
+      setHeaderData(res.data);
+    })();
+  }, []);
+
+  console.log(headerData);
+
   return (
     <div>
       <h1>Header</h1>
