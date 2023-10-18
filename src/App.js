d94704815cc53e79f242248f680237efd6b1fdef
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2023-10-18 04:06:05 +0200

    feat : create treeChart component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import { useEffect, useState } from "react";
 import Info from "./components/Info";
 import InputFilter from "./components/InputFilter";
 import "./style/global.css";
+import axios from "axios";
+import TreeChart from "./components/TreeChart";
 
 const App = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const res = await axios.get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d%2C14d%2C30d%2C200d%2C1y"
+      );
+      setData(res.data);
+    })();
+  }, []);
+
   return (
     <div>
       <h1 className="flex justify-center text-gray-100 font-extrabold text-3xl">
@@ -11,6 +25,7 @@ const App = () => {
       <header className="text-gray-100">
         <Info />
         <InputFilter />
+        <TreeChart data={data} />
       </header>
     </div>
   );
---
+++ src/components/TreeChart.jsx
@@ -0,0 +1,6 @@
+const TreeChart = ({ data }) => {
+  console.log(data);
+  return <div></div>;
+};
+
+export default TreeChart;
