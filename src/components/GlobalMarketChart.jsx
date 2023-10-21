b2edc606360971fcb4847f5a145cba0e9efeb449
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2023-10-21 23:29:11 +0200

    feat : get all necesesary data for the treeChart

--- src/components/GlobalMarketChart.jsx
+++ src/components/GlobalMarketChart.jsx
@@ -1,5 +1,28 @@
+import { useEffect, useState } from "react";
+
 const GlobalMarketChart = ({ data }) => {
-  return <div></div>;
+  const [dataArray, setDataArray] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      let chartData = [];
+
+      if (dataArray) {
+        for (let i = 0; i < 45; i++) {
+          chartData.push({
+            name: `${data[i].symbol.toUpperCase()} ${data[
+              i
+            ].market_cap_change_percentage_24h.toFixed(1)} %`,
+            size: `${data[i].market_cap}`,
+            fill: null,
+          });
+        }
+      }
+      console.log(chartData);
+    })();
+  }, [data, dataArray]);
+
+  return;
 };
 
 export default GlobalMarketChart;
