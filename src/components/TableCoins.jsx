cc0f0946b330d7813582a6993c7130a9814d511a
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2023-10-26 23:27:17 +0200

    feat : handle change for inpute text

--- src/components/TableCoins.jsx
+++ src/components/TableCoins.jsx
@@ -2,11 +2,15 @@ import React, { useState } from "react";
 
 const TableCoins = ({ data }) => {
   const [range, setRange] = useState(100);
+
+  const handleChange = (e) => {
+    setRange(e.target.value);
+  };
   return (
     <div>
       <div>
         <span>
-          top <input type="text" value={range}></input>
+          top <input type="text" value={range} onChange={handleChange}></input>
         </span>
       </div>
     </div>
