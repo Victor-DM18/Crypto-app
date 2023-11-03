71ef84a6866605099828ab592f87df50fa770ad8
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2023-11-03 18:30:27 +0100

    feat : create TableLine component

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import TableLine from "./TableLine";
 
 const Table = ({ data }) => {
   const [range, setRange] = useState(100);
@@ -62,21 +63,7 @@ const Table = ({ data }) => {
         ))}
       </div>
       <div className="text-xs lg:text-lg text-gray-100 flex flex-col mt-5 gap-2 ">
-        {data &&
-          data.slice(0, range).map((coin) => (
-            <ul className="flex flex-row gap-20  w-full border-t-2 border-gray-900 ">
-              <li className="flex justify-center w-8">
-                {coin.market_cap_rank}
-              </li>
-              <li className="flex justify-center w-8">{coin.name}</li>
-              <li className="flex justify-center w-20">{coin.current_price}</li>
-              <li className="flex justify-center w-12">{coin.market_cap}</li>
-              <li className="flex justify-center w-20">{coin.total_volume}</li>
-              <li className="flex justify-center w-4">
-                {parseFloat(coin.price_change_percentage_1h_in_currency)}
-              </li>
-            </ul>
-          ))}
+        <TableLine data={data} range={range} />
       </div>
     </div>
   );
