2da73037214e967b1ff02c0b97ef43e3633e106e
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2023-11-04 19:50:22 +0100

    feat : clear code

--- src/components/TableLine.jsx
+++ src/components/TableLine.jsx
@@ -5,18 +5,16 @@ const TableLine = ({ data, range }) => {
     <div className="text-xs">
       {data &&
         data.slice(0, range).map((coin) => (
-          <ul className="flex flex-row  py-3 w-full border-t-2 border-gray-900 ">
-            <li className="flex  justify-center w-12">
-              {coin.market_cap_rank}
-            </li>
-            <li className="flex justify-start w-48">{coin.name}</li>
-            <li className="flex w-28">{coin.current_price}</li>
-            <li className="flex w-40 ">{coin.market_cap}</li>
-            <li className="flex w-28">{coin.total_volume}</li>
-            <li className="flex w-28">
+          <div className="flex flex-row  py-3 w-fdivl border-t-2 border-gray-900 ">
+            <p className="flex  justify-center w-12">{coin.market_cap_rank}</p>
+            <p className="flex justify-start w-48">{coin.name}</p>
+            <p className="flex w-28">{coin.current_price}</p>
+            <p className="flex w-40 ">{coin.market_cap}</p>
+            <p className="flex w-28">{coin.total_volume}</p>
+            <p className="flex w-28">
               {parseFloat(coin.price_change_percentage_1h_in_currency)}
-            </li>
-          </ul>
+            </p>
+          </div>
         ))}
     </div>
   );
