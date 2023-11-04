f8356de1f6137b35da79233eecdd15b7de0c099d
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2023-11-04 19:59:16 +0100

    feat : priceFormate function

--- src/components/TableLine.jsx
+++ src/components/TableLine.jsx
@@ -1,6 +1,17 @@
 import React from "react";
 
 const TableLine = ({ data, range }) => {
+  const priceFormate = (price) => {
+    if (Math.round(price).toString().length < 4) {
+      return new Intl.NumberFormat("us-US", {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 7,
+      }).format(price);
+    } else {
+      return price;
+    }
+  };
+
   return (
     <div className="text-xs">
       {data &&
@@ -8,7 +19,9 @@ const TableLine = ({ data, range }) => {
           <div className="flex flex-row  py-3 w-fdivl border-t-2 border-gray-900 ">
             <p className="flex  justify-center w-12">{coin.market_cap_rank}</p>
             <p className="flex justify-start w-48">{coin.name}</p>
-            <p className="flex w-28">{coin.current_price}</p>
+            <p className="flex w-28">
+              {priceFormate(coin.current_price).toLocaleString() + " $"}
+            </p>
             <p className="flex w-40 ">{coin.market_cap}</p>
             <p className="flex w-28">{coin.total_volume}</p>
             <p className="flex w-28">
