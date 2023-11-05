196d7a97d487e4d6f60ae78572373ecdf32f114a
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2023-11-05 12:56:22 +0100

    feat : fix format marketCap

--- src/components/TableLine.jsx
+++ src/components/TableLine.jsx
@@ -13,25 +13,27 @@ const TableLine = ({ data, range }) => {
   };
 
   const marketCapFormat = (num) => {
-    let newNum = String(num).split("").slice(0, -6);
+    let newNum = num.toString().split("").slice(0, -6);
 
-    return newNum;
+    return Number(newNum.join(""));
   };
 
   return (
     <div className="text-xs">
       {data &&
         data.slice(0, range).map((coin) => (
-          <div className="flex flex-row  py-3 w-fdivl border-t-2 border-gray-900 ">
+          <div className="flex flex-row  py-2 w-fdivl border-t-2 border-gray-900 ">
             <p className="flex  justify-center w-12">{coin.market_cap_rank}</p>
             <p className="flex justify-start w-48">{coin.name}</p>
             <p className="flex w-32">
               {priceFormate(coin.current_price).toLocaleString()} $
             </p>
-            <p className="flex w-36 ">{marketCapFormat(coin.market_cap)} M $</p>
+            <p className="flex w-36 ">
+              {marketCapFormat(coin.market_cap).toLocaleString()} M $
+            </p>
             <p className="flex w-28">{coin.total_volume}</p>
             <p className="flex w-28">
-              {parseFloat(coin.price_change_percentage_1h_in_currency)}
+              {coin.price_change_percentage_1h_in_currency}
             </p>
           </div>
         ))}
