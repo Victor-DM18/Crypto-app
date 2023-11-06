d0567d3ad9f9f97827ece87230014a5c898c38ef
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2023-11-06 01:56:33 +0100

    feat :addprice change in 24h

--- src/components/TableLine.jsx
+++ src/components/TableLine.jsx
@@ -23,7 +23,7 @@ const TableLine = ({ data, range, ...otherProps }) => {
     <div className="text-xs" {...otherProps}>
       {data &&
         data.slice(0, range).map((coin) => (
-          <div className="flex flex-row  py-2 w-fdivl border-t-2 border-gray-900 ">
+          <div className="flex flex-row  py-2  border-t-2 border-gray-900 ">
             <p className="flex  justify-center w-12">{coin.market_cap_rank}</p>
             <p className="flex justify-start w-44">{coin.name}</p>
             <p className="flex w-32">
@@ -33,32 +33,37 @@ const TableLine = ({ data, range, ...otherProps }) => {
               {marketCapFormat(coin.market_cap).toLocaleString()} M $
             </p>
             <p className="flex w-36">{coin.total_volume.toLocaleString()}</p>
-            <p className="flex w-32">
+            <p className="flex justify-start w-28">
               <HandlePercentChange
                 number={coin.price_change_percentage_1h_in_currency}
               />
             </p>
-            <p className="flex w-32">
+            <p className="flex justify-start w-32">
+              <HandlePercentChange
+                number={coin.price_change_percentage_24h_in_currency}
+              />
+            </p>
+            <p className="flex justify-start w-32">
               <HandlePercentChange
                 number={coin.price_change_percentage_7d_in_currency}
               />
             </p>
-            <p className="flex w-32">
+            <p className="flex justify-start w-32">
               <HandlePercentChange
                 number={coin.price_change_percentage_30d_in_currency}
               />
             </p>
-            <p className="flex w-32">
+            <p className="flex justify-start w-28">
               <HandlePercentChange
                 number={coin.price_change_percentage_200d_in_currency}
               />
             </p>
-            <p className="flex w-32">
+            <p className="flex justify-start w-32">
               <HandlePercentChange
                 number={coin.price_change_percentage_1y_in_currency}
               />
             </p>
-            <p className="flex w-32">
+            <p className="flex justify-start">
               <HandlePercentChange number={coin.ath_change_percentage} />
             </p>
           </div>
