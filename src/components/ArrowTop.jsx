2ec10f0db6314c90fbfac2f48e562c88f7cf96e4
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2023-11-07 00:57:03 +0100

    feat : add handleClick for arowTop

--- src/components/ArrowTop.jsx
+++ src/components/ArrowTop.jsx
@@ -2,9 +2,13 @@ import React from "react";
 import { ArrowUpCircleIcon } from "@heroicons/react/24/solid";
 
 const ArrowTop = () => {
+  const handleClick = () => {
+    window.scrollTo(0, 0);
+  };
+
   return (
     <div>
-      <ArrowUpCircleIcon className="h-16" />
+      <ArrowUpCircleIcon className="h-16" onClick={handleClick} />
     </div>
   );
 };
