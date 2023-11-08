de2654cb85803a52d92773f744fe83543f7a0fda
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2023-11-08 19:10:14 +0100

    feat : FormRadioModified

--- src/components/FormRadio.jsx
+++ src/components/FormRadio.jsx
@@ -1,45 +1,50 @@
 import classNames from "classnames";
-import { ErrorMessage, Field } from "formik";
+import { Field, ErrorMessage } from "formik";
 import { forwardRef } from "react";
 
 // eslint-disable-next-line react/display-name
 const FormRadio = forwardRef((props, ref) => {
   const {
-    name,
+    placeholder,
+    children,
+    className,
+    autocomplete,
     label,
     type,
-    placeholder,
-    autoComplete,
-    options,
-    children,
+    name,
     ...otherProps
   } = props;
 
   return (
     <Field name={name}>
       {({ field }) => (
-        <label className="text-primary flex flex-col text-sky-950">
-          <span>{label}</span>
-          <select
+        <label
+          className={classNames(
+            { "text-primary flex flex-col text-sky-950": type !== "radio" },
+            className
+          )}
+        >
+          {type === "radio" ? null : <span>{children}</span>}
+          <input
             type={type}
             name={name}
             placeholder={placeholder}
-            autoComplete={autoComplete}
+            autoComplete={autocomplete}
             {...field}
             className={classNames(
-              "relative w-64 appearance-none rounded-md border bg-gray-200 px-3 py-2 text-sky-950 placeholder-sky-950 focus:z-10 focus:border-sky-700 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+              {
+                "relative w-64 appearance-none rounded-md border bg-gray-200 px-3 py-2 text-sky-950 placeholder-sky-950 focus:z-10 focus:border-sky-700 focus:outline-none focus:ring-sky-500 sm:text-sm":
+                  type !== "radio",
+              },
+              { hidden: type === "radio" }
             )}
             {...otherProps}
             ref={ref}
-          >
-            {Object.entries(options).map(([key, value]) => (
-              <option key={key} value={value}>
-                {key}
-              </option>
-            ))}
-          </select>
+          />
+          {label}
+
+          {type !== "radio" ? null : <span>{children}</span>}
           <ErrorMessage name={name} className="text-red-600" component="span" />
-          <span>{children}</span>
         </label>
       )}
     </Field>
