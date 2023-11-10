f5e923fe0381fd5ee9e4c5bfd55c9fba9ae626be
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2023-11-10 21:21:02 +0100

    feat : replace switch by if esle

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -72,88 +72,85 @@ const Table = ({ data }) => {
           data
             .slice(0, range)
             .sort((a, b) => {
-              switch (tri) {
-                case "Prix":
-                  return b.current_price - a.current_price;
-                case "Volume":
-                  return b.total_volume - a.total_volume;
-                case "MarketCap":
-                  return b.market_cap - a.market_cap;
-                case "1h":
-                  return (
-                    b.price_change_percentage_1h_in_currency -
-                    a.price_change_percentage_1h_in_currency
-                  );
-                case "1j":
-                  return (
-                    b.market_cap_change_percentage_24h -
-                    a.market_cap_change_percentage_24h
-                  );
-                case "1s":
-                  return (
-                    b.price_change_percentage_7d_in_currency -
-                    a.price_change_percentage_7d_in_currency
-                  );
-                case "1m":
-                  return (
-                    b.price_change_percentage_30d_in_currency -
-                    a.price_change_percentage_30d_in_currency
-                  );
-                case "6m":
-                  return (
-                    b.price_change_percentage_200d_in_currency -
-                    a.price_change_percentage_200d_in_currency
-                  );
-                case "1a":
-                  return (
-                    b.price_change_percentage_1y_in_currency -
-                    a.price_change_percentage_1y_in_currency
-                  );
-                case "ATH":
-                  return b.ath_change_percentage - a.ath_change_percentage;
-                case "#reverse":
-                  return a.market_cap - b.market_cap;
-                case "Prixreverse":
-                  return a.current_price - b.current_price;
-                case "Volumereverse":
-                  return a.total_volume - b.total_volume;
-                case "MarketCapreverse":
-                  return a.market_cap - b.market_cap;
-                case "1hreverse":
-                  return (
-                    a.price_change_percentage_1h_in_currency -
-                    b.price_change_percentage_1h_in_currency
-                  );
-                case "1jreverse":
-                  return (
-                    a.market_cap_change_percentage_24h -
-                    b.market_cap_change_percentage_24h
-                  );
-                case "1sreverse":
-                  return (
-                    a.price_change_percentage_7d_in_currency -
-                    b.price_change_percentage_7d_in_currency
-                  );
-                case "1mreverse":
-                  return (
-                    a.price_change_percentage_30d_in_currency -
-                    b.price_change_percentage_30d_in_currency
-                  );
-                case "6mreverse":
-                  return (
-                    a.price_change_percentage_200d_in_currency -
-                    b.price_change_percentage_200d_in_currency
-                  );
-                case "1areverse":
-                  return (
-                    a.price_change_percentage_1y_in_currency -
-                    b.price_change_percentage_1y_in_currency
-                  );
-                case "ATHreverse":
-                  return a.ath_change_percentage - b.ath_change_percentage;
-                default:
-                  return null;
-              }
+              if (tri === "Prix") {
+                return b.current_price - a.current_price;
+              } else if (tri === "Volume") {
+                return b.total_volume - a.total_volume;
+              } else if (tri === "MarketCap") {
+                return b.market_cap - a.market_cap;
+              } else if (tri === "1h") {
+                return (
+                  b.price_change_percentage_1h_in_currency -
+                  a.price_change_percentage_1h_in_currency
+                );
+              } else if (tri === "1j") {
+                return (
+                  b.market_cap_change_percentage_24h -
+                  a.market_cap_change_percentage_24h
+                );
+              } else if (tri === "1s") {
+                return (
+                  b.price_change_percentage_7d_in_currency -
+                  a.price_change_percentage_7d_in_currency
+                );
+              } else if (tri === "1m") {
+                return (
+                  b.price_change_percentage_30d_in_currency -
+                  a.price_change_percentage_30d_in_currency
+                );
+              } else if (tri === "6m") {
+                return (
+                  b.price_change_percentage_200d_in_currency -
+                  a.price_change_percentage_200d_in_currency
+                );
+              } else if (tri === "1a") {
+                return (
+                  b.price_change_percentage_1y_in_currency -
+                  a.price_change_percentage_1y_in_currency
+                );
+              } else if (tri === "ATH") {
+                return b.ath_change_percentage - a.ath_change_percentage;
+              } else if (tri === "#reverse") {
+                return a.market_cap - b.market_cap;
+              } else if (tri === "Prixreverse") {
+                return a.current_price - b.current_price;
+              } else if (tri === "Volumereverse") {
+                return a.total_volume - b.total_volume;
+              } else if (tri === "MarketCapreverse") {
+                return a.market_cap - b.market_cap;
+              } else if (tri === "1hreverse") {
+                return (
+                  a.price_change_percentage_1h_in_currency -
+                  b.price_change_percentage_1h_in_currency
+                );
+              } else if (tri === "1jreverse") {
+                return (
+                  a.market_cap_change_percentage_24h -
+                  b.market_cap_change_percentage_24h
+                );
+              } else if (tri === "1sreverse") {
+                return (
+                  a.price_change_percentage_7d_in_currency -
+                  b.price_change_percentage_7d_in_currency
+                );
+              } else if (tri === "1mreverse") {
+                return (
+                  a.price_change_percentage_30d_in_currency -
+                  b.price_change_percentage_30d_in_currency
+                );
+              } else if (tri === "6mreverse") {
+                return (
+                  a.price_change_percentage_200d_in_currency -
+                  b.price_change_percentage_200d_in_currency
+                );
+              } else if (tri === "1areverse") {
+                return (
+                  a.price_change_percentage_1y_in_currency -
+                  b.price_change_percentage_1y_in_currency
+                );
+              } else if (tri === "ATHreverse") {
+                return a.ath_change_percentage - b.ath_change_percentage;
+              } else return null;
             })
             .map((coin) => (
               <div className="odd:bg-gray-800">
