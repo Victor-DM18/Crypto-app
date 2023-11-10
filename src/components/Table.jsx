a3b3d3d3ca68957ff9da766831f269632a53396c
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2023-11-10 21:14:08 +0100

    feat : clean code

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -19,10 +19,6 @@ const Table = ({ data }) => {
     "ATH",
   ];
 
-  const handleTri = (e) => {
-    setTri(e.target.id);
-  };
-
   const handleChange = (e) => {
     setRange(e.target.value);
   };
@@ -53,7 +49,17 @@ const Table = ({ data }) => {
             <ul className="w-full flex flex-row gap-20 justify-around text-gray-100 text-center">
               {tableName.map((name, i) => (
                 <li key={name}>
-                  <input type="radio" id={name} className="hidden" />
+                  <input
+                    type="radio"
+                    id={name}
+                    className="hidden"
+                    onClick={() => {
+                      tri === name ? setTri(name + "reverse") : setTri(name);
+                    }}
+                    defaultChecked={
+                      name === tri || name === tri + "reverse" ? true : false
+                    }
+                  />
                   <label htmlFor={name}>{name}</label>
                 </li>
               ))}
@@ -63,11 +69,97 @@ const Table = ({ data }) => {
       </div>
       <div className="text-xs text-gray-100 flex flex-col mt-5 items-center">
         {data &&
-          data.slice(0, range).map((coin) => (
-            <div className="odd:bg-gray-800" key={coin} id={coin}>
-              <TableLine key={data.id} tri={tri} coin={coin} />
-            </div>
-          ))}
+          data
+            .slice(0, range)
+            .sort((a, b) => {
+              switch (tri) {
+                case "Prix":
+                  return b.current_price - a.current_price;
+                case "Volume":
+                  return b.total_volume - a.total_volume;
+                case "MarketCap":
+                  return b.market_cap - a.market_cap;
+                case "1h":
+                  return (
+                    b.price_change_percentage_1h_in_currency -
+                    a.price_change_percentage_1h_in_currency
+                  );
+                case "1j":
+                  return (
+                    b.market_cap_change_percentage_24h -
+                    a.market_cap_change_percentage_24h
+                  );
+                case "1s":
+                  return (
+                    b.price_change_percentage_7d_in_currency -
+                    a.price_change_percentage_7d_in_currency
+                  );
+                case "1m":
+                  return (
+                    b.price_change_percentage_30d_in_currency -
+                    a.price_change_percentage_30d_in_currency
+                  );
+                case "6m":
+                  return (
+                    b.price_change_percentage_200d_in_currency -
+                    a.price_change_percentage_200d_in_currency
+                  );
+                case "1a":
+                  return (
+                    b.price_change_percentage_1y_in_currency -
+                    a.price_change_percentage_1y_in_currency
+                  );
+                case "ATH":
+                  return b.ath_change_percentage - a.ath_change_percentage;
+                case "#reverse":
+                  return a.market_cap - b.market_cap;
+                case "Prixreverse":
+                  return a.current_price - b.current_price;
+                case "Volumereverse":
+                  return a.total_volume - b.total_volume;
+                case "MarketCapreverse":
+                  return a.market_cap - b.market_cap;
+                case "1hreverse":
+                  return (
+                    a.price_change_percentage_1h_in_currency -
+                    b.price_change_percentage_1h_in_currency
+                  );
+                case "1jreverse":
+                  return (
+                    a.market_cap_change_percentage_24h -
+                    b.market_cap_change_percentage_24h
+                  );
+                case "1sreverse":
+                  return (
+                    a.price_change_percentage_7d_in_currency -
+                    b.price_change_percentage_7d_in_currency
+                  );
+                case "1mreverse":
+                  return (
+                    a.price_change_percentage_30d_in_currency -
+                    b.price_change_percentage_30d_in_currency
+                  );
+                case "6mreverse":
+                  return (
+                    a.price_change_percentage_200d_in_currency -
+                    b.price_change_percentage_200d_in_currency
+                  );
+                case "1areverse":
+                  return (
+                    a.price_change_percentage_1y_in_currency -
+                    b.price_change_percentage_1y_in_currency
+                  );
+                case "ATHreverse":
+                  return a.ath_change_percentage - b.ath_change_percentage;
+                default:
+                  return null;
+              }
+            })
+            .map((coin) => (
+              <div className="odd:bg-gray-800">
+                <TableLine key={data.id} tri={tri} coin={coin} />
+              </div>
+            ))}
       </div>
     </div>
   );
