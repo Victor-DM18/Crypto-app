bf28f0fe262a7bd8eab32ccaa459be522f06ea18
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2023-11-12 22:33:53 +0100

    feat : icon create

--- src/components/FavorisIcon.jsx
+++ src/components/FavorisIcon.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
+import { StarIcon } from "@heroicons/react/24/outline";
+// import { StarIcon } from "@heroicons/react/24/outline";
+
 const FavorisIcon = () => {
-  return <div></div>;
+  return (
+    <div>
+      <StarIcon className="h-4" />
+    </div>
+  );
 };
 
 export default FavorisIcon;
