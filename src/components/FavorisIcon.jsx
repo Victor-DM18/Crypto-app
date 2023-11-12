d6a1ae44ae028942a278a3a705357663f94a2bfa
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2023-11-12 22:37:31 +0100

    feat : cursor-pointer hover icon + set handleClick

--- src/components/FavorisIcon.jsx
+++ src/components/FavorisIcon.jsx
@@ -2,10 +2,12 @@ import React from "react";
 
 import { StarIcon } from "@heroicons/react/24/solid";
 
+const handleClick = () => {};
+
 const FavorisIcon = () => {
   return (
     <div>
-      <StarIcon className="h-4" />
+      <StarIcon className="h-4 hover:cursor-pointer" onClick={handleClick} />
     </div>
   );
 };
