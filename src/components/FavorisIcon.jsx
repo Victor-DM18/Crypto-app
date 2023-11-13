671f59aab44441d7911e3a98c19316a59dfc508e
Victor DOS MARTIRES <victor.dosmartires@gmail.com>
2023-11-13 21:07:35 +0100

    feat : add className to StarIcon

--- src/components/FavorisIcon.jsx
+++ src/components/FavorisIcon.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 
 import { StarIcon } from "@heroicons/react/24/solid";
+import classNames from "classnames";
 
 const FavorisIcon = ({ coinId }) => {
   const [like, setLike] = useState(false);
@@ -19,7 +20,13 @@ const FavorisIcon = ({ coinId }) => {
   const handleClick = () => {};
   return (
     <div>
-      <StarIcon className="h-4 hover:cursor-pointer" onClick={handleClick} />
+      <StarIcon
+        className={classNames(
+          "h-4 hover:cursor-pointer",
+          like ? "text-cyan-500" : "text-gray-100"
+        )}
+        onClick={handleClick}
+      />
     </div>
   );
 };
